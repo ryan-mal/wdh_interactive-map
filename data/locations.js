9ea4a3b4171b0854647c69b548b74e9fab6280fd
Ryan Mallampati <ryanmallampati@gmail.com>
2024-07-04 17:33:30 +1000

    Added area field and updated description automation

--- data/locations.js
+++ data/locations.js
@@ -1,29 +1,32 @@
 var locations = [
     {
         name: "Trollskull Manor",
+		area: "Trollskull Alley",
         ward: "North Ward",
         type: ["Base", "Residence", "Inn_Tavern_Festhall"],
         coordinates: [62.714462, 21.291504],
         image: "images/Trollskull_Manor.png",
-        description: "<b>Trollskull Manor</b><br><i>Trollskull Alley, North Ward</i><p>Four stories tall and boasting balconies, a turret, and five chimneys, this Manorial House and Tavern was once one of the grandest estates in Trollskull Alley. It is now nothing more than a crumbling memory.",
+        description: "Four stories tall and boasting balconies, a turret, and five chimneys, this Manorial House and Tavern was once one of the grandest estates in Trollskull Alley. It is now nothing more than a crumbling memory.",
         notableInfo: ""
     },
     {
         name: "Blackstaff Tower",
+		area: "Blackstaff Academy",
         ward: "Castle Ward",
         type: ["Academy"],
         coordinates: [29.113775, -39.023438],
         image: "images/blackstaff_tower.png",
-        description: "<b>Blackstaff Tower</b><br><i>Blackstaff Academy, Castle Ward</i><p>Previously known as Arunsun Tower, Blackstaff Tower stands proud as the Blackstaff Academy's main library, and the headquarters of the Grey Hands. The tower was the home of Khelben \"Blackstaff\" Arunsun as well as of his successors to the title of Blackstaff. The foreboding structure, made of smooth black onyx, is one of Waterdeep's most recognizable landmarks.",
+        description: "Previously known as Arunsun Tower, Blackstaff Tower stands proud as the Blackstaff Academy's main library, and the headquarters of the Grey Hands. The tower was the home of Khelben \"Blackstaff\" Arunsun as well as of his successors to the title of Blackstaff. The foreboding structure, made of smooth black onyx, is one of Waterdeep's most recognizable landmarks.",
         notableInfo: ""
     },
     {
         name: "Archway Commons",
+		area: "Blackstaff Academy",
         ward: "Castle Ward",
         type: ["Miscellaneous"],
         coordinates: [30.145127, -31.508789],
         image: "images/archway_commons.png",
-        description: "<b>Archway Commons</b><br><i>Blackstaff Academy, Castle Ward</i><p>Just east of Blackstaff Tower lies one of Waterdeep's few public parks. More familiarly called the Commons, the lush park is a favourite spot for students of Blackstaff Academy to gather in fair weather or to wander through on dates. Standing proud at its centre is a statue of Khelben Arunsun, the first Blackstaff.",
+        description: "Just east of Blackstaff Tower lies one of Waterdeep's few public parks. More familiarly called the Commons, the lush park is a favourite spot for students of Blackstaff Academy to gather in fair weather or to wander through on dates. Standing proud at its centre is a statue of Khelben Arunsun, the first Blackstaff.",
         notableInfo: ""
     },
 
